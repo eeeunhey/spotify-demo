cda83ecb6fdeec6103b659a4ff18bbb693d4148f
eunhye <dmsgp2627@naver.com>
2025-12-22 00:37:30 +0900

    이동 버튼 추가

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,15 +1,19 @@
-import { Outlet } from "react-router"
-
+import { Link, Outlet } from "react-router";
 
 const AppLayout = () => {
   return (
     <div>
-        
-        <h1>Sidebar</h1>
-        {/* 자식컴포넌트를 Outlet에서 보여준다 */}
-        <Outlet />
+      <h1>Sidebar</h1>
+      <Link to="/">
+        <button>Home</button>
+      </Link>
+      <Link to="/search">
+        <button>Search</button>
+      </Link>
+      {/* 자식컴포넌트를 Outlet에서 보여준다 */}
+      <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default AppLayout
+export default AppLayout;
